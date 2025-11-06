fe286306a678db84042f894d4a3eea22217c5b76
Prashant Srivastav <p_srivastav@outlook.com>
2025-11-06 21:31:19 +0800

    feat: added ui into docker componse

--- docker/postgres/init.sql
+++ docker/postgres/init.sql
@@ -1,11 +1,9 @@
--- Create databases for each service
-CREATE DATABASE IF NOT EXISTS web_db;
-CREATE DATABASE IF NOT EXISTS assistant_db;
-CREATE DATABASE IF NOT EXISTS integration_db;
-CREATE DATABASE IF NOT EXISTS endpoint_db;
+-- Create databases
+CREATE DATABASE assistant_db;
+CREATE DATABASE integration_db;
+CREATE DATABASE endpoint_db;
 
--- Grant privileges to rapida_user on all databases
-GRANT ALL PRIVILEGES ON DATABASE web_db TO rapida_user;
+-- Grant privileges
 GRANT ALL PRIVILEGES ON DATABASE assistant_db TO rapida_user;
 GRANT ALL PRIVILEGES ON DATABASE integration_db TO rapida_user;
 GRANT ALL PRIVILEGES ON DATABASE endpoint_db TO rapida_user;
