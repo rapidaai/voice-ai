ad4499dbd849e0c2f5dec9055229161cc7b42fa3
Prashant Srivastav <p_srivastav@outlook.com>
2026-08-10 14:28:42 +0530

    fix: updated agentkit blocking and unblocking policy

--- api/assistant-api/internal/llm/agentkit/agentkit_stream.go
+++ api/assistant-api/internal/llm/agentkit/agentkit_stream.go
@@ -118,7 +118,6 @@ func (e *agentkitExecutor) Write(ctx context.Context, comm internal_type.Communi
 		})
 
 	case *protos.TalkOutput_Control:
-		e.logger.Debugf("controling packet with %+v", data)
 		if data.Control == nil {
 			return
 		}
@@ -127,21 +126,14 @@ func (e *agentkitExecutor) Write(ctx context.Context, comm internal_type.Communi
 		}
 		switch data.Control.GetAction() {
 		case protos.ConversationControl_CONTROL_ACTION_BLOCK:
-			e.logger.Debugf("controling packet with %+v", data)
 			for _, controlType := range data.Control.GetTypes() {
 				switch controlType {
 				case protos.ConversationControl_CONTROL_TYPE_USER_AUDIO:
-					comm.OnPacket(ctx, internal_type.DispatchPolicyPacket{
-						ContextID: data.Control.GetId(),
-						Policy: internal_type.DispatchPolicy{
-							Target: internal_type.PacketNameUserAudioReceived,
-							Action: internal_type.DispatchActionIgnore,
-						},
-					},
+					comm.OnPacket(ctx,
 						internal_type.DispatchPolicyPacket{
 							ContextID: data.Control.GetId(),
 							Policy: internal_type.DispatchPolicy{
-								Target: internal_type.PacketNameInterruptionDetected,
+								Target: internal_type.PacketNameUserAudioReceived,
 								Action: internal_type.DispatchActionIgnore,
 							},
 						})
@@ -151,13 +143,6 @@ func (e *agentkitExecutor) Write(ctx context.Context, comm internal_type.Communi
 							ContextID: data.Control.GetId(),
 							Policy: internal_type.DispatchPolicy{
 								Target: internal_type.PacketNameUserTextReceived,
-								Action: internal_type.DispatchActionIgnore,
-							},
-						},
-						internal_type.DispatchPolicyPacket{
-							ContextID: data.Control.GetId(),
-							Policy: internal_type.DispatchPolicy{
-								Target: internal_type.PacketNameInterruptionDetected,
 								Action: internal_type.DispatchActionIgnore,
 							},
 						})
@@ -198,21 +183,37 @@ func (e *agentkitExecutor) Write(ctx context.Context, comm internal_type.Communi
 						},
 					})
 				case protos.ConversationControl_CONTROL_TYPE_USER_TEXT:
-					comm.OnPacket(ctx, internal_type.DispatchPolicyPacket{
-						ContextID: data.Control.GetId(),
-						Policy: internal_type.DispatchPolicy{
-							Target: internal_type.PacketNameUserTextReceived,
-							Action: internal_type.DispatchActionPassthrough,
-						},
-					})
+					comm.OnPacket(ctx,
+						internal_type.DispatchPolicyPacket{
+							ContextID: data.Control.GetId(),
+							Policy: internal_type.DispatchPolicy{
+								Target: internal_type.PacketNameUserTextReceived,
+								Action: internal_type.DispatchActionPassthrough,
+							},
+						})
 				case protos.ConversationControl_CONTROL_TYPE_BARGE_IN:
-					comm.OnPacket(ctx, internal_type.DispatchPolicyPacket{
-						ContextID: data.Control.GetId(),
-						Policy: internal_type.DispatchPolicy{
-							Target: internal_type.PacketNameInterruptionDetected,
-							Action: internal_type.DispatchActionPassthrough,
+					comm.OnPacket(ctx,
+						internal_type.DispatchPolicyPacket{
+							ContextID: data.Control.GetId(),
+							Policy: internal_type.DispatchPolicy{
+								Target: internal_type.PacketNameUserAudioReceived,
+								Action: internal_type.DispatchActionPassthrough,
+							},
 						},
-					})
+						internal_type.DispatchPolicyPacket{
+							ContextID: data.Control.GetId(),
+							Policy: internal_type.DispatchPolicy{
+								Target: internal_type.PacketNameUserTextReceived,
+								Action: internal_type.DispatchActionPassthrough,
+							},
+						},
+						internal_type.DispatchPolicyPacket{
+							ContextID: data.Control.GetId(),
+							Policy: internal_type.DispatchPolicy{
+								Target: internal_type.PacketNameInterruptionDetected,
+								Action: internal_type.DispatchActionPassthrough,
+							},
+						})
 				}
 			}
 		}
